56c452784b601eb30179c13a97f5b68e274c08c1
Gökçen Öğütçü <gokcen@brewww.com>
2014-10-28 12:52:29 +0200

    Added sample local.php

---
+++ sample/local.php
@@ -0,0 +1,9 @@
+<?php
+$config['db'] = array(
+	'connection_string' => 'mysql:host=127.0.0.1;dbname=foo',
+	'username' => 'foo',
+	'password' => 'bar',
+);
+$config['debug'] = true;
+$config['twig'] = array();
+return $config;
