3a8b9342ad94e8c74951bdc405869f485337f78d
Gökçen Öğütçü <gokceno@users.noreply.github.com>
2014-10-28 12:45:56 +0200

    config/local.php removed
    This file should be ignored as it contains sensitive information. A sample will be placed under samples/ folder

--- config/local.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['db'] = array(
-	'connection_string' => 'mysql:host=127.0.0.1;dbname=breview',
-	'username' => 'root',
-	'password' => 'root',
-);
-$config['debug'] = true;
-$config['twig'] = array();
-return $config;
